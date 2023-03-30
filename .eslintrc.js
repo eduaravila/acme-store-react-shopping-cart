ad88ef3d80ab099435d2396b54e09b8f157b9cdd
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2023-03-30 02:17:29 -0600

    fix: lint action update

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    "next/core-web-vitals",
+    "eslint:recommended", // https://eslint.org/docs/latest/rules/ this includes all the ✅
+    "prettier",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["./tsconfig.json"],
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["react", "unused-imports", "@typescript-eslint"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+  },
+};
